473a6e16c9ebe3cdd7cfdde7a8872252b7fffef3
ming.ma <isdom.maming@gmail.com>
2016-01-21 14:20:43 +0800

    添加 Producer 工具类: ONSProducerUtil

---
+++ src/main/java/org/jocean/aliyun/ons/ONSProducerUtil.java
@@ -0,0 +1,46 @@
+package org.jocean.aliyun.ons;
+
+import java.io.UnsupportedEncodingException;
+
+import org.jocean.aliyun.ons.ONSProducer;
+import org.jocean.idiom.ExceptionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.aliyun.openservices.ons.api.Message;
+import com.aliyun.openservices.ons.api.Producer;
+import com.aliyun.openservices.ons.api.SendResult;
+
+public class ONSProducerUtil {
+    
+    private static final Logger LOG = LoggerFactory.getLogger(ONSProducer.class);
+    
+    private static final byte[] EMPTY_BYTES = new byte[0];
+    
+    public static void send (final String topicId,final Producer producer,final String content) {
+        byte[] body = EMPTY_BYTES;
+        try {
+            body = content.getBytes("utf-8");
+            Message msg = new Message(
+                    //Message Topic
+                    topicId,
+                    //Message Tag,
+                    //可理解为Gmail中的标签，对消息进行再归类，方便Consumer指定过滤条件在ONS服务器过滤       
+                    topicId,
+                    //Message Body
+                    //任何二进制形式的数据，ONS不做任何干预，需要Producer与Consumer协商好一致的序列化和反序列化方式
+                    topicId + System.currentTimeMillis(),
+                    body
+                    );
+            //发送消息，只要不抛异常就是成功
+            final SendResult sendResult = producer.send(msg);
+            if (LOG.isInfoEnabled()) {
+                LOG.info("{}", sendResult);
+            }
+        } catch (UnsupportedEncodingException e) {
+            LOG.warn("exception when content.getBytes, content:{}, detail: {}", 
+                    content, ExceptionUtils.exception2detail(e));
+        }
+    }
+
+}

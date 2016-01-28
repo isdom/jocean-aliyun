4ea8bea8b3b376add54bba08b5327feeadea1521
ming.ma <isdom.maming@gmail.com>
2016-01-28 17:45:58 +0800

    对消费ONS Topic 所触发业务流程记录其 endReason及执行耗时

--- src/main/java/org/jocean/aliyun/ons/ONSConsumer.java
+++ src/main/java/org/jocean/aliyun/ons/ONSConsumer.java
@@ -1,14 +1,19 @@
 package org.jocean.aliyun.ons;
 
 import java.util.Properties;
+import java.util.concurrent.atomic.AtomicReference;
 
 import javax.inject.Inject;
 
+import org.jocean.event.api.EndReasonAware;
 import org.jocean.event.api.EventEngine;
 import org.jocean.event.api.EventReceiver;
+import org.jocean.event.api.FlowLifecycleListener;
 import org.jocean.event.api.internal.EventHandler;
 import org.jocean.idiom.BeanHolder;
 import org.jocean.idiom.BeanHolderAware;
+import org.jocean.idiom.StopWatch;
+import org.jocean.j2se.stats.FlowStats;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -42,19 +47,21 @@ public class ONSConsumer implements BeanHolderAware{
         this._topicId = topicId;
     }
 
+    @SuppressWarnings({ "unchecked", "rawtypes" })
     public void start(){
-        Properties properties = new Properties();
+        final Properties properties = new Properties();
         properties.put(PropertyKeyConst.ConsumerId, _consumerId);
         properties.put(PropertyKeyConst.AccessKey, _accessKey);
         properties.put(PropertyKeyConst.SecretKey, _secretKey);
         properties.put(PropertyKeyConst.ConsumeThreadNums,2);
-        _consumer = ONSFactory.createConsumer(properties);
-        _consumer.subscribe(_topicId, "*", new MessageListener() {
+        this._consumer = ONSFactory.createConsumer(properties);
+        this._consumer.subscribe(this._topicId, "*", new MessageListener() {
             public Action consume(Message message, ConsumeContext context) {
                 return invokeFlow(message);
             }
         });
-        _consumer.start();
+        this._consumer.start();
+        this._stats.addFlows(this._topicId, "SUBSCRIBE", (Class)this._flowCls);
         LOG.info("Consumer Started");
     }
     
@@ -95,18 +102,49 @@ public class ONSConsumer implements BeanHolderAware{
 
     private Action invokeFlow(final Message message) {
         final Object flow = this._beanHolder.getBean(this._flowCls);
-        try {
-            final EventReceiver receiver = this._engine.create(
-                    flow.getClass().getSimpleName(),
-                    (EventHandler)flow.getClass().getField(_initState).get(flow),
-                    flow);
-            receiver.acceptEvent(this._event, message);
-            if (LOG.isDebugEnabled()) {
-                LOG.debug("Receive: {}", message);
+        if (null!=flow) {
+            final StopWatch clock = new StopWatch();
+            final AtomicReference<String> endReasonRef = new AtomicReference<>("default");
+            try {
+                final EventReceiver receiver = this._engine.create(
+                        flow.getClass().getSimpleName(),
+                        (EventHandler)flow.getClass().getField(_initState).get(flow),
+                        flow,
+                        new EndReasonAware() {
+                            @Override
+                            public void setEndReason(final Object endreason) {
+                                endReasonRef.set(endreason.toString());
+                            }},
+                        new FlowLifecycleListener() {
+                            @Override
+                            public void afterEventReceiverCreated(final EventReceiver receiver)
+                                    throws Exception {
+                            }
+
+                            @Override
+                            public void afterFlowDestroy() throws Exception {
+                                final int count = _stats.incExecutedCount(_flowCls);
+                                _stats.recordExecutedInterval(_flowCls, endReasonRef.get(), clock.stopAndRestart());
+                                if (LOG.isDebugEnabled()) {
+                                    LOG.debug("{}'s afterFlowDestroy, so record biz count: {}", 
+                                            flow, count);
+//                                    LOG.debug("and all flows biz record:\n{}", Arrays.toString(getFlows()));
+                                }
+                            }}                        );
+                receiver.acceptEvent(this._event, message);
+                if (LOG.isDebugEnabled()) {
+                    LOG.debug("Receive: {}", message);
+                }
+            } catch (Exception e) {
+                LOG.warn("exception when get field({}) for {}", _initState, flow);
             }
-        } catch (Exception e) {
-            LOG.warn("exception when get field({}) for {}", _initState, flow);
+            return Action.CommitMessage;
+        } else {
+            LOG.warn("can't get flow instance for class:{}, reply ONS with ReconsumeLater", this._flowCls);
+            return Action.ReconsumeLater;
         }
-        return Action.CommitMessage;
     }
+    
+    @Inject
+    private FlowStats _stats;
 }

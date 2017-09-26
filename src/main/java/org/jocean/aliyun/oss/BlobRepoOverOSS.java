6b5f8e51f65d15e33a8457976f99c52ee7191856
ming.ma <isdom.maming@gmail.com>
2017-09-26 09:44:23 +0800

    implement True RxJava style's putObject API

--- src/main/java/org/jocean/aliyun/oss/BlobRepoOverOSS.java
+++ src/main/java/org/jocean/aliyun/oss/BlobRepoOverOSS.java
@@ -2,9 +2,20 @@ package org.jocean.aliyun.oss;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.InetSocketAddress;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 import javax.inject.Inject;
 
+import org.jocean.aliyun.oss.internal.OSSRequestSigner;
+import org.jocean.http.Feature;
+import org.jocean.http.client.HttpClient;
+import org.jocean.http.client.HttpClient.HttpInitiator;
+import org.jocean.http.util.HttpMessageHolder;
+import org.jocean.http.util.RxNettys;
 import org.jocean.idiom.ExceptionUtils;
 import org.jocean.netty.BlobRepo;
 import org.slf4j.Logger;
@@ -17,15 +28,56 @@ import com.aliyun.oss.model.ObjectMetadata;
 import com.aliyun.oss.model.PutObjectResult;
 import com.google.common.io.ByteStreams;
 
+import io.netty.handler.codec.http.DefaultHttpRequest;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpMethod;
+import io.netty.handler.codec.http.HttpObject;
+import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http.HttpVersion;
+//import io.netty.handler.codec.http.HttpContent;
 import rx.Observable;
 import rx.Observable.OnSubscribe;
 import rx.Subscriber;
+import rx.functions.Action0;
 import rx.functions.Func0;
+import rx.functions.Func1;
+import rx.subscriptions.Subscriptions;
 
 public class BlobRepoOverOSS implements BlobRepo {
     
     private static final Logger LOG = 
         LoggerFactory.getLogger(BlobRepoOverOSS.class);
+    
+    public Observable<String> putObject(
+            final String objname,
+            final Func0<? extends Blob> blobProducer) {
+        return Observable.unsafeCreate(new OnSubscribe<String>() {
+            @Override
+            public void call(final Subscriber<? super String> subscriber) {
+                if (!subscriber.isUnsubscribed()) {
+                    final Blob blob = blobProducer.call();
+
+                    if (null != blob) {
+                        // ensure blob will be release when
+                        // subscriber.unsubscribe
+                        subscriber.add(Subscriptions.create(new Action0() {
+                            @Override
+                            public void call() {
+                                final boolean released = blob.release();
+                                LOG.debug("{} unsubscribe cause call blob {}'s release with return {}", subscriber,
+                                        blob, released);
+                            }
+                        }));
+
+                        buildPutAPI(hostWithBucketname(), objname, blob).subscribe(subscriber);
+                    } else {
+                        subscriber.onError(new RuntimeException("can't produce blob"));
+                    }
+                }
+            }
+        });
+    }
     
     @Override
     public Observable<PutResult> putBlob(final String key, 
@@ -176,8 +228,88 @@ public class BlobRepoOverOSS implements BlobRepo {
         this._ossclient = ossclient;
     }
     
+    private Observable<String> buildPutAPI(
+            final String host, 
+            final String objname, 
+            final Blob blob) {
+        final HttpRequest request = new DefaultHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.PUT,
+                "/" + objname);
+
+        // Host header
+        request.headers().set(HttpHeaderNames.HOST, host);
+
+        // Date header
+        request.headers().set(HttpHeaderNames.DATE, buildGMT4Now(new Date()));
+
+        // Content-Length header
+        request.headers().set(HttpHeaderNames.CONTENT_LENGTH, blob.contentLength());
+
+        // Content-Type header
+        request.headers().set(HttpHeaderNames.CONTENT_TYPE, blob.contentType());
+        
+        new OSSRequestSigner( "/" + _bucketName + "/" + objname, 
+                _ossclient.getCredentialsProvider().getCredentials()).sign(request);
+        
+        return _httpclient.initiator().remoteAddress(new InetSocketAddress(host, 80))
+            .feature(Feature.ENABLE_LOGGING)
+            .build()
+            .flatMap(callAPI(buildObsRequest(request, blob)));
+    }
+
+    private Func1<HttpInitiator, Observable<String>> callAPI(final Observable<HttpObject> obsRequest) {
+        return new Func1<HttpInitiator, Observable<String>>() {
+            @Override
+            public Observable<String> call(final HttpInitiator initiator) {
+                final HttpMessageHolder holder = new HttpMessageHolder();
+                initiator.doOnTerminate(holder.closer());
+                return initiator.defineInteraction(obsRequest)
+                        .compose(holder.<HttpObject>assembleAndHold())
+                        .last()
+                        .flatMap(new Func1<HttpObject, Observable<String>>() {
+                            @Override
+                            public Observable<String> call(final HttpObject any) {
+                                final FullHttpResponse fhr = holder.fullOf(RxNettys.BUILD_FULL_RESPONSE).call();
+                                if (null != fhr) {
+                                    try {
+//                                        return Observable.just(new String(
+//                                            ByteStreams.toByteArray(new ByteBufInputStream(fhr.content())), 
+//                                            CharsetUtil.UTF_8));
+                                        return Observable.just(fhr.headers().get(HttpHeaderNames.ETAG));
+                                    }
+//                                    catch (IOException e) {
+//                                        return Observable.error(e);
+//                                    }
+                                    finally {
+                                        fhr.release();
+                                    }
+                                }
+                                return Observable.error(new RuntimeException("can't get response"));
+                            }})
+                        .doOnUnsubscribe(initiator.closer());
+            }};
+    }
+
+    private Observable<HttpObject> buildObsRequest(final HttpRequest request, final Blob blob) {
+        return Observable.concat( 
+                    Observable.just(request), 
+                    blob.content());
+    }
+
+    private String hostWithBucketname() {
+        return this._bucketName + "." + this._ossclient.getEndpoint().getHost();
+    }
+
+    private static String buildGMT4Now(final Date date) {
+        final SimpleDateFormat sdf = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss 'GMT'", Locale.US);
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT")); // 设置时区为GMT
+        return sdf.format(date);
+    }
+
     @Inject
     private OSSClient _ossclient;
+    
+    @Inject
+    private HttpClient _httpclient;
     
     private String _bucketName;
 }
--- src/main/java/org/jocean/aliyun/oss/internal/OSSRequestSigner.java
+++ src/main/java/org/jocean/aliyun/oss/internal/OSSRequestSigner.java
@@ -21,32 +21,28 @@ package org.jocean.aliyun.oss.internal;
 
 import com.aliyun.oss.ClientException;
 import com.aliyun.oss.common.auth.Credentials;
-import com.aliyun.oss.common.auth.RequestSigner;
 import com.aliyun.oss.common.auth.ServiceSignature;
-import com.aliyun.oss.common.comm.RequestMessage;
 
 import io.netty.handler.codec.http.HttpRequest;
 
 public class OSSRequestSigner {
 
-    private String httpMethod;
-    
     /* Note that resource path should not have been url-encoded. */
     private String resourcePath;
     private Credentials creds;
 
-    public OSSRequestSigner(String httpMethod, String resourcePath, Credentials creds) {
-        this.httpMethod = httpMethod;
+    public OSSRequestSigner(final String resourcePath, final Credentials creds) {
         this.resourcePath = resourcePath;
         this.creds = creds;
     }
 
-    public void sign(HttpRequest request) throws ClientException {
+    public void sign(final HttpRequest request) throws ClientException {
         String accessKeyId = creds.getAccessKeyId();
         String secretAccessKey = creds.getSecretAccessKey();
 
         if (accessKeyId.length() > 0 && secretAccessKey.length() > 0) {
-            final String canonicalString = SignUtils.buildCanonicalString(httpMethod, resourcePath, request, null);
+            final String canonicalString = SignUtils.buildCanonicalString(
+                    request.method().toString(), resourcePath, request, null);
             final String signature = ServiceSignature.create().computeSignature(secretAccessKey, canonicalString);
             request.headers().add(OSSHeaders.AUTHORIZATION, 
                 OSSUtils.composeRequestAuthorization(accessKeyId, signature));

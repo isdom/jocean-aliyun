a7839e3bead0999ace41fa1d2f27f91bc467e0f3
ming.ma <isdom.maming@gmail.com>
2018-06-17 21:59:35 +0800

    更新 api 方法形式为: Func1<Interact, Observable<? extends ImageInfo>> info(final String pathToImage)

--- src/main/java/org/jocean/aliyun/oss/OSSImageService.java
+++ src/main/java/org/jocean/aliyun/oss/OSSImageService.java
@@ -1,8 +1,10 @@
 package org.jocean.aliyun.oss;
 
+import org.jocean.http.Interact;
 import org.jocean.netty.BlobRepo.Blob;
 
 import rx.Observable;
+import rx.functions.Func1;
 
 public interface OSSImageService {
     public interface ImageInfo {
@@ -11,26 +13,26 @@ public interface OSSImageService {
         public int imageWidth();
         public int imageHeight();
     }
-    
-    public Observable<? extends ImageInfo> info(final String pathToImage);
-    
+
+    public Func1<Interact, Observable<? extends ImageInfo>> info(final String pathToImage);
+
     public Observable<? extends Blob> process(final String pathToImage, final String actions);
-    
+
     public interface ProcessAction {
         public ActionSet and();
     }
-    
+
     public interface Info extends ProcessAction {
-        
+
     }
-    
+
     public interface Crop extends ProcessAction {
         public Crop x(final int x);
         public Crop y(final int y);
         public Crop w(final int w);
         public Crop h(final int h);
     }
-    
+
     public interface Resize extends ProcessAction {
         public Resize m(final String m);
         public Resize w(final int w);
@@ -38,14 +40,14 @@ public interface OSSImageService {
         public Resize limit(final int limit);
         public Resize color(final String color);
     }
-    
+
     public interface ActionSet {
         public Info info();
         public Crop crop();
         public Resize resize();
         public String build();
     }
-    
+
     public ActionSet actions();
-    
+
 }
--- src/main/java/org/jocean/aliyun/oss/impl/OSSImageServiceImpl.java
+++ src/main/java/org/jocean/aliyun/oss/impl/OSSImageServiceImpl.java
@@ -12,6 +12,8 @@ import org.jocean.aliyun.oss.spi.GetImageContentResponse;
 import org.jocean.aliyun.oss.spi.GetImageInfoResponse;
 import org.jocean.aliyun.oss.spi.GetImageWithProcessRequest;
 import org.jocean.http.Feature;
+import org.jocean.http.Interact;
+import org.jocean.http.MessageUtil;
 import org.jocean.http.rosa.SignalClient;
 import org.jocean.idiom.BeanFinder;
 import org.jocean.idiom.ExceptionUtils;
@@ -21,71 +23,62 @@ import org.slf4j.LoggerFactory;
 
 import com.google.common.collect.Lists;
 
+import io.netty.handler.codec.http.HttpMethod;
 import rx.Observable;
+import rx.functions.Func1;
 
 public class OSSImageServiceImpl implements OSSImageService {
-    private static final Logger LOG = 
+    private static final Logger LOG =
             LoggerFactory.getLogger(OSSImageServiceImpl.class);
 
     @Override
-    public Observable<? extends ImageInfo> info(final String pathToImage) {
-        try {
-            final GetImageWithProcessRequest req = new GetImageWithProcessRequest();
-            req.setPathToImage(pathToImage);
-            req.setProcessActions(actions().info().and().build());
-            final URI uri = new URI("http://" + _bucketName + "." + _endpoint);
-            return this._finder.find(SignalClient.class).flatMap(signal -> signal.interaction().request(req)
-                    .feature(Feature.ENABLE_LOGGING)
-                    .feature(Feature.ENABLE_COMPRESSOR)
-                    .feature(new SignalClient.UsingMethod(GET.class))
-                    .feature(new SignalClient.UsingUri(uri))
-                    .feature(new SignalClient.DecodeResponseBodyAs(GetImageInfoResponse.class))
-                    .<GetImageInfoResponse>build())
-                    .<ImageInfo>map(resp -> new ImageInfo() {
-                                @Override
-                                public String toString() {
-                                    final StringBuilder builder = new StringBuilder();
-                                    builder.append("ImageInfo [fileSize=").append(fileSize())
-                                            .append(", format=").append(format())
-                                            .append(", imageWidth=").append(imageWidth())
-                                            .append(", imageHeight=").append(imageHeight())
-                                            .append("]");
-                                    return builder.toString();
-                                }
-                                
-                                @Override
-                                public long fileSize() {
-                                    return resp.getFileSize();
-                                }
+    public Func1<Interact, Observable<? extends ImageInfo>> info(final String pathToImage) {
+        final GetImageWithProcessRequest req = new GetImageWithProcessRequest();
+        req.setPathToImage(pathToImage);
+        req.setProcessActions(actions().info().and().build());
+        final String uri = "http://" + _bucketName + "." + _endpoint;
+        return interact -> interact.reqbean(req).uri(uri).method(HttpMethod.GET).execution()
+                .compose(MessageUtil.responseAs(GetImageInfoResponse.class, MessageUtil::unserializeAsJson))
+                .<ImageInfo>map(resp -> new ImageInfo() {
+                    @Override
+                    public String toString() {
+                        final StringBuilder builder = new StringBuilder();
+                        builder.append("ImageInfo [fileSize=").append(fileSize()).append(", format=").append(format())
+                                .append(", imageWidth=").append(imageWidth()).append(", imageHeight=")
+                                .append(imageHeight()).append("]");
+                        return builder.toString();
+                    }
 
-                                @Override
-                                public String format() {
-                                    return resp.getFormat();
-                                }
+                    @Override
+                    public long fileSize() {
+                        return resp.getFileSize();
+                    }
 
-                                @Override
-                                public int imageWidth() {
-                                    return resp.getImageWidth();
-                                }
+                    @Override
+                    public String format() {
+                        return resp.getFormat();
+                    }
 
-                                @Override
-                                public int imageHeight() {
-                                    return resp.getImageHeight();
-                                }});
-        } catch (URISyntaxException e) {
-            LOG.warn("exception when signalClient.defineInteraction, detail: {}",
-                    ExceptionUtils.exception2detail(e));
-            return Observable.error(e);
-        }
+                    @Override
+                    public int imageWidth() {
+                        return resp.getImageWidth();
+                    }
+
+                    @Override
+                    public int imageHeight() {
+                        return resp.getImageHeight();
+                    }
+                });
     }
 
+    @Override
     public Observable<? extends Blob> process(final String pathToImage, final String actions) {
         try {
             final GetImageWithProcessRequest req = new GetImageWithProcessRequest();
             req.setPathToImage(pathToImage);
             req.setProcessActions(actions);
             final URI uri = new URI("http://" + _bucketName + "." + _endpoint);
-            
+
             return this._finder.find(SignalClient.class).flatMap(signal -> signal.interaction().request(req)
                     .feature(Feature.ENABLE_LOGGING)
                     .feature(Feature.ENABLE_COMPRESSOR)
@@ -99,13 +92,13 @@ public class OSSImageServiceImpl implements OSSImageService {
 //                            final String contentDisposition = resp.contentDisposition();
                             return Blob.Util.fromByteArray(content, contentType, null, null);
                         });
-        } catch (URISyntaxException e) {
+        } catch (final URISyntaxException e) {
             LOG.warn("exception when signalClient.defineInteraction, detail: {}",
                     ExceptionUtils.exception2detail(e));
             return Observable.error(e);
         }
     }
-    
+
     public void setEndpoint(final String endpoint) {
         this._endpoint = endpoint;
     }
@@ -116,23 +109,23 @@ public class OSSImageServiceImpl implements OSSImageService {
 
     @Inject
     private BeanFinder _finder;
-    
+
     private String _endpoint;
-    
+
     private String _bucketName;
 
     @Override
     public ActionSet actions() {
         return new DefaultActionSet();
     }
-    
+
     static class DefaultActionSet implements ActionSet {
 
         @Override
         public Info info() {
             return new DefaultInfo(this);
         }
-        
+
         @Override
         public Crop crop() {
             return new DefaultCrop(this);
@@ -142,30 +135,30 @@ public class OSSImageServiceImpl implements OSSImageService {
         public Resize resize() {
             throw new UnsupportedOperationException();
         }
-        
+
         @Override
         public String build() {
             final StringBuilder sb = new StringBuilder();
             sb.append("image");
-            for (String actionAndParams : this._actions) {
+            for (final String actionAndParams : this._actions) {
                 sb.append('/');
                 sb.append(actionAndParams);
             }
             return sb.toString();
         }
-        
+
         void addAction(final String actionAndParams) {
             this._actions.add(actionAndParams);
         }
-        
+
         final List<String> _actions = Lists.newArrayList();
     }
-    
+
     static abstract class BaseAction implements ProcessAction {
         BaseAction(final DefaultActionSet actionSet) {
             this._actionSet = actionSet;
         }
-        
+
         public ActionSet addToActionSet(final String actionAndParams) {
             this._actionSet.addAction(actionAndParams);
             return this._actionSet;
@@ -178,15 +171,15 @@ public class OSSImageServiceImpl implements OSSImageService {
                 sb.append(value);
             }
         }
-        
+
         private final DefaultActionSet _actionSet;
     }
-    
+
     static class DefaultInfo extends BaseAction implements Info {
         DefaultInfo(final DefaultActionSet actionSet) {
             super(actionSet);
         }
-        
+
         @Override
         public ActionSet and() {
             final StringBuilder sb = new StringBuilder();
@@ -194,13 +187,13 @@ public class OSSImageServiceImpl implements OSSImageService {
             return addToActionSet(sb.toString());
         }
     }
-        
-    static class DefaultCrop extends BaseAction 
+
+    static class DefaultCrop extends BaseAction
         implements Crop {
         DefaultCrop(final DefaultActionSet actionSet) {
             super(actionSet);
         }
-        
+
         @Override
         public ActionSet and() {
             final StringBuilder sb = new StringBuilder();
@@ -235,7 +228,7 @@ public class OSSImageServiceImpl implements OSSImageService {
             this._h = Integer.toString(h);
             return this;
         }
-        
+
         private String _x = null;
         private String _y = null;
         private String _w = null;

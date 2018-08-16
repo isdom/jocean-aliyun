9984af1db8566b13068e6545ffd269b8d94d51d6
ming.ma <isdom@qq.com>
2018-08-16 10:23:13 +0200

    fix "bad return type in lambda expression" Error

--- src/main/java/org/jocean/aliyun/oss/BlobRepoOverOSS.java
+++ src/main/java/org/jocean/aliyun/oss/BlobRepoOverOSS.java
@@ -102,7 +102,7 @@ public class BlobRepoOverOSS implements BlobRepo {
                 .onrequest(signRequest(objname))
                 .execution()
                 .flatMap(execution -> execution.execute())
-                .flatMap(resp -> {
+                .<MessageBody>flatMap(resp -> {
                     // https://help.aliyun.com/document_detail/32005.html?spm=a2c4g.11186623.6.1090.DeJEv5
                     final String contentType = resp.message().headers().get(HttpHeaderNames.CONTENT_TYPE);
                     if (null != contentType && contentType.startsWith("application/xml")) {

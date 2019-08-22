e78a7c6c19b4605131b7f0c4024c15148915f397
ming.ma <isdom@qq.com>
2019-08-22 21:22:28 +0800

    add SlbAPI interface

---
+++ src/main/java/org/jocean/aliyun/slb/SlbAPI.java
@@ -0,0 +1,26 @@
+package org.jocean.aliyun.slb;
+
+import com.alibaba.fastjson.annotation.JSONField;
+
+public interface SlbAPI {
+
+    interface SLBAPIResponse {
+        @JSONField(name="RequestId")
+        String getRequestId();
+
+        @JSONField(name="RequestId")
+        void setRequestId(final String requestId);
+
+        @JSONField(name="Code")
+        String getCode();
+
+        @JSONField(name="Code")
+        void setCode(final String code);
+
+        @JSONField(name="Message")
+        String getMessage();
+
+        @JSONField(name="Message")
+        void setMessage(final String message);
+    }
+}

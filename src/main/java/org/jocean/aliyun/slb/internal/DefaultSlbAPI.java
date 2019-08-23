c026cc833f8d14b1e760df424f46e2a92127ad38
ming.ma <isdom@qq.com>
2019-08-23 21:52:59 +0800

    完善 SlbAPI 定义

---
+++ src/main/java/org/jocean/aliyun/slb/internal/DefaultSlbAPI.java
@@ -0,0 +1,9 @@
+package org.jocean.aliyun.slb.internal;
+
+import org.jocean.aliyun.slb.SlbAPI;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DefaultSlbAPI implements SlbAPI {
+    private static final Logger LOG = LoggerFactory.getLogger(DefaultSlbAPI.class);
+}

e8b3deb3610173903fb6969a0dc0227960c38993
ming.ma <isdom@qq.com>
2019-10-12 10:12:29 +0800

    add ModifyPrepayInstanceSpec API, TBD: https://help.aliyun.com/document_detail/57612.html?spm=a2c4g.11186623.6.1176.ce5913babKE89L

--- src/main/java/org/jocean/aliyun/ecs/EcsAPI.java
+++ src/main/java/org/jocean/aliyun/ecs/EcsAPI.java
@@ -1192,4 +1192,22 @@ public interface EcsAPI {
     }
 
     ModifyInstanceSpecBuilder modifyInstanceSpec();
+
+    interface ModifyPrepayInstanceSpecResponse extends ECSAPIResponse {
+    }
+
+    interface ModifyPrepayInstanceSpecBuilder {
+        @QueryParam("InstanceIds")
+        DetachInstanceRamRoleBuilder instanceIds(final String[] instanceIds);
+
+        @QueryParam("RamRoleName")
+        DetachInstanceRamRoleBuilder ramRoleName(final String ramRoleName);
+
+        @QueryParam("RegionId")
+        DetachInstanceRamRoleBuilder regionId(final String regionId);
+
+        Transformer<RpcRunner, ModifyPrepayInstanceSpecResponse> call();
+    }
+
+    ModifyPrepayInstanceSpecBuilder modifyPrepayInstanceSpec();
 }
--- src/main/java/org/jocean/aliyun/ecs/internal/DefaultEcsAPI.java
+++ src/main/java/org/jocean/aliyun/ecs/internal/DefaultEcsAPI.java
@@ -269,6 +269,12 @@ public class DefaultEcsAPI implements EcsAPI {
         return null;
     }
 
+    @Override
+    public ModifyPrepayInstanceSpecBuilder modifyPrepayInstanceSpec() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
 //    @Value("${role}")
 //    String _roleName = null;
 

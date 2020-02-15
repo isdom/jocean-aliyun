08fea54b2224ce265fe8aeae25f8443086d02cfc
ming.ma <isdom@qq.com>
2020-02-15 21:27:23 +0800

    增加直接应用于 Interact 的 AKSigner2 类型定义及实现，并一并在 aliyun_aksigner 中创建实例

---
+++ src/main/java/org/jocean/aliyun/sign/AKSigner2.java
@@ -0,0 +1,18 @@
+package org.jocean.aliyun.sign;
+
+import org.jocean.http.Interact;
+import org.springframework.beans.factory.annotation.Value;
+
+public class AKSigner2 implements AliyunSigner2 {
+
+    @Override
+    public void call(final Interact interact) {
+        interact.onsending(SignerV1.signRequest(_ak_id, _ak_secret));
+    }
+
+    @Value("${ak_id}")
+    String _ak_id;
+
+    @Value("${ak_secret}")
+    String _ak_secret;
+}
---
+++ src/main/java/org/jocean/aliyun/sign/AliyunSigner2.java
@@ -0,0 +1,9 @@
+package org.jocean.aliyun.sign;
+
+import org.jocean.http.Interact;
+
+import rx.functions.Action1;
+
+public interface AliyunSigner2 extends Action1<Interact>{
+
+}

b498744083f4b5d914af2a7e0039fd9e0c9cd599
ming.ma <isdom@qq.com>
2020-02-20 17:22:35 +0800

    在 SLb API 中备注 @Produces 型注解的支持形式

--- src/main/java/org/jocean/aliyun/slb/SlbAPI.java
+++ src/main/java/org/jocean/aliyun/slb/SlbAPI.java
@@ -23,4 +23,10 @@ public interface SlbAPI {
         @JSONField(name="Message")
         void setMessage(final String message);
     }
+
+    // https://help.aliyun.com/document_detail/35215.html?spm=a2c4g.11186623.6.742.22274267oYul4v
+
+    // TODO : CreateVServerGroup: 调用CreateVServerGroup向指定的后端服务器组中添加后端服务器。
+//  @Produces(MediaType.APPLICATION_JSON)
+//  SlbXXXBuilder content(final SlbRequest content);
 }

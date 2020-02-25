65bd0af63737e0d873110465ba7f5b9008952805
ming.ma <isdom@qq.com>
2020-02-25 20:51:07 +0800

    fix bug: String.split(".") --> String.split("\\.")

--- src/main/java/org/jocean/aliyun/sign/Signer4OSS.java
+++ src/main/java/org/jocean/aliyun/sign/Signer4OSS.java
@@ -60,7 +60,7 @@ public class Signer4OSS {
         final String host = req.headers().get(HttpHeaderNames.HOST);
         LOG.debug("sign4oss: host:{}", host);
 
-        final String bucketName = host.split(".")[0];
+        final String bucketName = host.split("\\.")[0];
 
         final QueryStringDecoder decoder = new QueryStringDecoder(req.uri());
         final String objectName = decoder.path();

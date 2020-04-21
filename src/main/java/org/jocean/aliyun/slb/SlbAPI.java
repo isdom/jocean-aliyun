27ab4d595b4c72d2c39f378d4e1bb1db32224e75
ming.ma <isdom@qq.com>
2020-04-21 21:01:27 +0800

    add createLoadBalancer API for SlbAPI

--- src/main/java/org/jocean/aliyun/slb/SlbAPI.java
+++ src/main/java/org/jocean/aliyun/slb/SlbAPI.java
@@ -1,6 +1,12 @@
 package org.jocean.aliyun.slb;
 
+import javax.ws.rs.GET;
+
+import org.jocean.rpc.annotation.ConstParams;
+
 import com.alibaba.fastjson.annotation.JSONField;
+
+import rx.Observable;
 
 public interface SlbAPI {
 
@@ -23,6 +29,20 @@ public interface SlbAPI {
         @JSONField(name="Message")
         void setMessage(final String message);
     }
+
+    interface CreateLoadBalancerResponse extends SLBAPIResponse {
+
+    }
+
+    // https://help.aliyun.com/document_detail/27577.html?spm=a2c4g.11186623.6.704.63de1771E9lN47
+    interface CreateLoadBalancerBuilder {
+
+        @GET
+        @ConstParams({"Action", "CreateLoadBalancer"})
+        Observable<CreateLoadBalancerResponse> call();
+    }
+
+    public CreateLoadBalancerBuilder createLoadBalancer();
 
     // https://help.aliyun.com/document_detail/35215.html?spm=a2c4g.11186623.6.742.22274267oYul4v
 
--- src/main/java/org/jocean/aliyun/slb/internal/DefaultSlbAPI.java
+++
@@ -1,9 +0,0 @@
-package org.jocean.aliyun.slb.internal;
-
-import org.jocean.aliyun.slb.SlbAPI;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class DefaultSlbAPI implements SlbAPI {
-    private static final Logger LOG = LoggerFactory.getLogger(DefaultSlbAPI.class);
-}

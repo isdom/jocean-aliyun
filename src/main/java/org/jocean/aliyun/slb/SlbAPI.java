a9905350eea301eda045ee9da59b6f97a646c2b0
ming.ma <isdom@qq.com>
2020-11-18 17:44:41 +0800

    补全 SetLoadBalancerHTTPListenerAttributeBuilder 属性

--- src/main/java/org/jocean/aliyun/slb/SlbAPI.java
+++ src/main/java/org/jocean/aliyun/slb/SlbAPI.java
@@ -1556,8 +1556,263 @@ public interface SlbAPI {
         @QueryParam("LoadBalancerId")
         SetLoadBalancerHTTPListenerAttributeBuilder loadBalancerId(final String loadBalancerId);
 
+        /**
+         * 是否开启健康检查。
+            取值：on | off。
+         */
+        @QueryParam("HealthCheck")
+        SetLoadBalancerHTTPListenerAttributeBuilder healthCheck(final String healthCheck);
+
         @QueryParam("ListenerPort")
         SetLoadBalancerHTTPListenerAttributeBuilder listenerPort(final int listenerPort);
+
+        /**
+         * 是否开启会话保持。
+            取值：on | off。
+         */
+        @QueryParam("StickySession")
+        SetLoadBalancerHTTPListenerAttributeBuilder stickySession(final String stickySession);
+
+        /**
+         * cookie的处理方式。取值：
+
+            insert：植入Cookie。
+            客户端第一次访问时，负载均衡会在返回请求中植入Cookie（即在HTTP/HTTPS响应报文中插入SERVERID），下次客户端携带此Cookie访问，负载均衡服务会将请求定向转发给之前记录到的后端服务器上。
+
+            server：重写Cookie。
+            负载均衡发现用户自定义了Cookie，将会对原来的Cookie进行重写，下次客户端携带新的Cookie访问，负载均衡服务会将请求定向转发给之前记录到的后端服务器。
+            说明 当StickySession的值为on时，必须指定该参数。
+         */
+        @QueryParam("StickySessionType")
+        SetLoadBalancerHTTPListenerAttributeBuilder stickySessionType(final String stickySessionType);
+
+        /**
+         * 监听的带宽峰值，取值：
+            -1：不限制带宽峰值。
+            1~5120：监听的带宽峰值，所有监听的带宽峰值之和不能超过实例的带宽峰值。
+         */
+        @QueryParam("Bandwidth")
+        SetLoadBalancerHTTPListenerAttributeBuilder bandwidth(final int bandwidth);
+
+        /**
+         * 负载均衡实例后端使用的端口。
+            取值：1~65535。
+            说明 如果不使用服务器组（不指定VServerGroupId参数），则该参数必选。
+         */
+        @QueryParam("BackendServerPort")
+        SetLoadBalancerHTTPListenerAttributeBuilder backendServerPort(final int backendServerPort);
+
+        /**
+         * 是否开启通过X-Forwarded-For头字段获取来访者真实 IP。
+            取值为on。
+         */
+        @QueryParam("XForwardedFor")
+        SetLoadBalancerHTTPListenerAttributeBuilder xForwardedFor(final String xForwardedFor);
+
+        /**
+         * 调度算法。取值：
+            wrr（默认值）：权重值越高的后端服务器，被轮询到的次数（概率）也越高。
+            wlc：除了根据每台后端服务器设定的权重值来进行轮询，同时还考虑后端服务器的实际负载（即连接数）。当权重值相同时，当前连接数越小的后端服务器被轮询到的次数（概率）也越高。
+            rr：按照访问顺序依次将外部请求依序分发到后端服务器。
+         */
+        @QueryParam("Scheduler")
+        SetLoadBalancerHTTPListenerAttributeBuilder scheduler(final String scheduler);
+
+        /**
+         * Cookie超时时间。
+            取值：1~86400（秒）。
+            说明 当StickySession为on且StickySessionType为insert时，该参数必选。
+         */
+        @QueryParam("CookieTimeout")
+        SetLoadBalancerHTTPListenerAttributeBuilder cookieTimeout(final int cookieTimeout);
+
+        /**
+         * 服务器上配置的Cookie。
+            长度为1~200个字符，只能包含ASCII英文字母和数字字符，不能包含逗号、分号或空格，也不能以$开头。
+            说明 当StickySession为on且StickySessionType为server时，该参数必选。
+         */
+        @QueryParam("Cookie")
+        SetLoadBalancerHTTPListenerAttributeBuilder cookie(final String cookie);
+
+        /**
+         * 用于健康检查的域名，取值：
+            $_ip： 后端服务器的私网IP。当指定了IP或该参数未指定时，负载均衡会使用各后端服务器的私网IP当做健康检查使用的域名。
+            domain：域名长度为1-80字符，只能包含字母、数字、点号（.）和连字符（-）。
+            说明 在HealthCheck值为on时才会有效。
+         */
+        @QueryParam("HealthCheckDomain")
+        SetLoadBalancerHTTPListenerAttributeBuilder healthCheckDomain(final String healthCheckDomain);
+
+        /**
+         * 用于健康检查的URI。
+
+            长度限制为1~80，只能使用字母、数字和-/.%?#&amp;这些字符。 URL不能只为/，但必须以/开头。
+            说明 在HealthCheck值为on时才会有效。
+         */
+        @QueryParam("HealthCheckURI")
+        SetLoadBalancerHTTPListenerAttributeBuilder healthCheckURI(final String healthCheckURI);
+
+        /**
+         * 健康检查连续成功多少次后，将后端服务器的健康检查状态由fail判定为success。
+
+            取值：2~10。
+            说明 在HealthCheck值为on时才会有效。
+         */
+        @QueryParam("HealthyThreshold")
+        SetLoadBalancerHTTPListenerAttributeBuilder healthyThreshold(final int healthyThreshold);
+
+        /**
+         * 健康检查连续失败多少次后，将后端服务器的健康检查状态由success判定为fail。
+
+            取值：2~10。
+            说明 在HealthCheck值为on时才会有效。
+         */
+        @QueryParam("UnhealthyThreshold")
+        SetLoadBalancerHTTPListenerAttributeBuilder unhealthyThreshold(final int unhealthyThreshold);
+
+        /**
+         * 接收来自运行状况检查的响应需要等待的时间。如果后端ECS在指定的时间内没有正确响应，则判定为健康检查失败。在HealthCheck值为on时才会有效。
+
+            取值：1~300（秒）。
+            说明 如果HealthCHeckTimeout的值小于HealthCheckInterval的值，则HealthCHeckTimeout无效，超时时间为HealthCheckInterval的值。
+         */
+        @QueryParam("HealthCheckTimeout")
+        SetLoadBalancerHTTPListenerAttributeBuilder healthCheckTimeout(final int healthCheckTimeout);
+
+        /**
+         * 健康检查的后端服务器的端口。
+
+            取值： 1~65535。
+            说明 在HealthCheck值为on时才会有效。
+         */
+        @QueryParam("HealthCheckConnectPort")
+        SetLoadBalancerHTTPListenerAttributeBuilder healthCheckConnectPort(final int healthCheckConnectPort);
+
+        /**
+         * 健康检查的时间间隔。
+
+            取值： 1~50（秒）。
+            说明 在HealthCheck值为on时才会有效。
+         */
+        @QueryParam("HealthCheckInterval")
+        SetLoadBalancerHTTPListenerAttributeBuilder healthCheckInterval(final int healthCheckInterval);
+
+        /**
+         * 健康检查正常的HTTP状态码，多个状态码用逗号分隔。
+
+            默认值为http_2xx。
+            取值：http_2xx | http_3xx | http_4xx | http_5xx。
+            说明 在HealthCheck值为on时才会有效。
+         */
+        @QueryParam("HealthCheckHttpCode")
+        SetLoadBalancerHTTPListenerAttributeBuilder healthCheckHttpCode(final String healthCheckHttpCode);
+
+        /**
+         * 虚拟服务器组ID。
+         */
+        @QueryParam("VServerGroupId")
+        SetLoadBalancerHTTPListenerAttributeBuilder vServerGroupId(final String vServerGroupId);
+
+        /**
+         * 是否通过SLB-IP头字段获取客户端请求的真实IP。
+
+            取值：on | off（默认值）。
+         */
+        @QueryParam("XForwardedFor_SLBIP")
+        SetLoadBalancerHTTPListenerAttributeBuilder xForwardedFor_SLBIP(final String xForwardedFor_SLBIP);
+
+        /**
+         * 是否通过SLB-ID头字段获取负载均衡实例ID。
+
+            取值：on | off（默认值）。
+         */
+        @QueryParam("XForwardedFor_SLBID")
+        SetLoadBalancerHTTPListenerAttributeBuilder xForwardedFor_SLBID(final String xForwardedFor_SLBID);
+
+        /**
+         * 是否通过X-Forwarded-Proto头字段获取负载均衡实例的监听协议。
+
+            取值：on | off（默认值）。
+         */
+        @QueryParam("XForwardedFor_proto")
+        SetLoadBalancerHTTPListenerAttributeBuilder xForwardedFor_proto(final String xForwardedFor_proto);
+
+        /**
+         * 是否开启Gzip压缩，对特定文件类型进行压缩。默认值为on。
+
+            取值：on | off。
+         */
+        @QueryParam("Gzip")
+        SetLoadBalancerHTTPListenerAttributeBuilder gzip(final String gzip);
+
+        /**
+         * 监听绑定的访问策略组ID。
+
+            当AclStatus参数的值为on时，该参数必选。
+         */
+        @QueryParam("AclId")
+        SetLoadBalancerHTTPListenerAttributeBuilder aclId(final String aclId);
+
+        /**
+         * 访问控制类型：
+
+            white： 仅转发来自所选访问控制策略组中设置的IP地址或地址段的请求，白名单适用于应用只允许特定IP访问的场景。
+            设置白名单存在一定业务风险。
+            一旦设置白名单，就只有白名单中的IP可以访问负载均衡监听。
+            如果开启了白名单访问，但访问策略组中没有添加任何IP，则负载均衡监听不会转发请求。
+
+            black： 来自所选访问控制策略组中设置的IP地址或地址段的所有请求都不会转发，黑名单适用于应用只限制某些特定IP访问的场景。
+            如果开启了黑名单访问，但访问策略组中没有添加任何IP，则负载均衡监听会转发全部请求。
+            当AclStatus参数的值为on时，该参数有效。
+         */
+        @QueryParam("AclType")
+        SetLoadBalancerHTTPListenerAttributeBuilder aclType(final String aclType);
+
+        /**
+         * 是否开启访问控制功能。
+
+            取值：on | off（默认值）。
+         */
+        @QueryParam("AclStatus")
+        SetLoadBalancerHTTPListenerAttributeBuilder aclStatus(final String aclStatus);
+
+        /**
+         * 设置监听的描述信息。
+
+            长度限制为1-80个字符，允许包含字母、数字、短横线（-）、正斜杠（/）、点号（.）和下划线（_）等字符。支持中文描述。
+         */
+        @QueryParam("Description")
+        SetLoadBalancerHTTPListenerAttributeBuilder description(final String description);
+
+        /**
+         * 是否开启HTTP至HTTPS的转发。
+
+            取值：on | off。
+         */
+        @QueryParam("ListenerForward")
+        SetLoadBalancerHTTPListenerAttributeBuilder listenerForward(final String listenerForward);
+
+        /**
+         * HTTP至HTTPS的监听转发端口。
+         */
+        @QueryParam("ForwardPort")
+        SetLoadBalancerHTTPListenerAttributeBuilder forwardPort(final int forwardPort);
+
+        /**
+         * 指定连接空闲超时时间，取值范围为1~60秒，默认值为15秒。
+
+            在超时时间内一直没有访问请求，负载均衡会暂时中断当前连接，直到一下次请求来临时重新建立新的连接。
+         */
+        @QueryParam("IdleTimeout")
+        SetLoadBalancerHTTPListenerAttributeBuilder idleTimeout(final int idleTimeout);
+
+        /**
+         * 指定请求超时时间，取值范围为1~180秒，默认值为60秒。
+
+            在超时时间内后端服务器一直没有响应，负载均衡将放弃等待，给客户端返回 HTTP 504 错误码。
+         */
+        @QueryParam("RequestTimeout")
+        SetLoadBalancerHTTPListenerAttributeBuilder requestTimeout(final int requestTimeout);
 
         @GET
         @ConstParams({"Action", "SetLoadBalancerHTTPListenerAttribute"})

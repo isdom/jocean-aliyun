657473b9b2961d326907862ae3f99c8a46ff0819
ming.ma <isdom@qq.com>
2020-12-10 19:19:06 +0800

    完善 CreateLoadBalancerHTTPSListener API

--- src/main/java/org/jocean/aliyun/slb/SlbAPI.java
+++ src/main/java/org/jocean/aliyun/slb/SlbAPI.java
@@ -1878,8 +1878,307 @@ public interface SlbAPI {
         @QueryParam("LoadBalancerId")
         CreateLoadBalancerHTTPSListenerBuilder loadBalancerId(final String loadBalancerId);
 
+        /**
+         *
+        负载均衡实例前端使用的端口。
+        取值：1-65535。
+         */
         @QueryParam("ListenerPort")
         CreateLoadBalancerHTTPSListenerBuilder listenerPort(final int listenerPort);
+
+        /**
+         *
+        监听的带宽峰值。
+        取值：-1 | 1-5120。
+        -1：对于按流量计费的公网负载均衡实例，可以将带宽峰值设置为-1，即不限制带宽峰值。
+        1-5120(Mbps)： 对于按带宽计费的公网负载均衡实例，可以设置每个监听的带宽峰值，但所有监听的带宽峰值之和不能超过实例的带宽峰值。
+         */
+        @QueryParam("Bandwidth")
+        CreateLoadBalancerHTTPSListenerBuilder bandwidth(final int bandwidth);
+
+        /**
+         *
+         * 服务器证书的ID。
+         */
+        @QueryParam("ServerCertificateId")
+        CreateLoadBalancerHTTPSListenerBuilder serverCertificateId(final String serverCertificateId);
+
+        /**
+         *
+        是否开启会话保持。
+        取值：on | off。
+         */
+        @QueryParam("StickySession")
+        CreateLoadBalancerHTTPSListenerBuilder stickySession(final String stickySession);
+
+        /**
+         *
+        是否开启健康检查。
+        取值：on | off。
+         */
+        @QueryParam("HealthCheck")
+        CreateLoadBalancerHTTPSListenerBuilder healthCheck(final String healthCheck);
+
+        /**
+        *
+        负载均衡实例后端使用的端口，取值：1-65535。
+        如果不使用服务器组（不指定VServerGroupId），则该参数必选。
+        */
+        @QueryParam("BackendServerPort")
+        CreateLoadBalancerHTTPSListenerBuilder backendServerPort(final int backendServerPort);
+
+        /**
+        *
+        是否通过X-Forwarded-For获取客户端请求的真实IP。
+        取值为on。
+        */
+        @QueryParam("XForwardedFor")
+        CreateLoadBalancerHTTPSListenerBuilder xForwardedFor(final String xForwardedFor);
+
+        /**
+        *
+        调度算法。取值：
+        wrr（默认值）：权重值越高的后端服务器，被轮询到的次数（概率）也越高。
+        wlc：除了根据每台后端服务器设定的权重值来进行轮询，同时还考虑后端服务器的实际负载（即连接数）。当权重值相同时，当前连接数越小的后端服务器被轮询到的次数（概率）也越高。
+        rr：按照访问顺序依次将外部请求依序分发到后端服务器。
+        */
+        @QueryParam("Scheduler")
+        CreateLoadBalancerHTTPSListenerBuilder scheduler(final String scheduler);
+
+        /**
+        *
+        Cookie的处理方式。当StickySession的值为on时，必须指定该参数。
+        取值：insert | server。
+        insert：植入Cookie。
+        客户端第一次访问时，负载均衡会在返回请求中植入Cookie（即在HTTP/HTTPS响应报文中插入SERVERID），下次客户端携带此Cookie访问，负载均衡服务会将请求定向转发给之前记录到的后端服务器上。
+        server：重写Cookie。
+        负载均衡发现用户自定义了Cookie，将会对原来的Cookie进行重写，下次客户端携带新的Cookie访问，负载均衡服务会将请求定向转发给之前记录到的后端服务器。
+        */
+        @QueryParam("StickySessionType")
+        CreateLoadBalancerHTTPSListenerBuilder stickySessionType(final String stickySessionType);
+
+        /**
+        *
+        Cookie超时时间。
+        取值：1-86400（秒）。
+        说明 当StickySession为on且StickySessionType为insert时，该参数必选。
+        */
+        @QueryParam("CookieTimeout")
+        CreateLoadBalancerHTTPSListenerBuilder cookieTimeout(final int cookieTimeout);
+
+        /**
+        *
+        服务器上配置的Cookie。
+        长度为1-200，只能包含ASCII英文字母和数字字符，不能包含逗号、分号或空格，也不能以$开头。
+        说明 当StickySession为on且StickySessionType为server时，该参数必选。
+        */
+        @QueryParam("Cookie")
+        CreateLoadBalancerHTTPSListenerBuilder cookie(final String cookie);
+
+        /**
+        *
+        用于健康检查的域名。取值：
+        $_ip： 后端服务器的私网IP。当指定了IP或该参数未指定时，负载均衡会使用各后端服务器的私网IP当做健康检查使用的域名。
+        domain：域名长度为1~80，只能包含字母、数字、点号（.）和连字符（-）。
+        说明 在HealthCheck值为on时才会有效。
+        */
+        @QueryParam("HealthCheckDomain")
+        CreateLoadBalancerHTTPSListenerBuilder healthCheckDomain(final String healthCheckDomain);
+
+        /**
+        *
+        用于健康检查的URI。
+        长度限制为1~80，只能使用字母、数字和-/.%?#&这些字符。 URL不能只为/，但必须以/开头。
+        说明 在HealthCheck值为on时才会有效。
+        */
+        @QueryParam("HealthCheckURI")
+        CreateLoadBalancerHTTPSListenerBuilder healthCheckURI(final String healthCheckURI);
+
+        /**
+        *
+        健康检查连续成功多少次后，将后端服务器的健康检查状态由fail判定为success。
+        取值：2-10。
+        说明 在HealthCheck值为on时才会有效。
+        */
+        @QueryParam("HealthyThreshold")
+        CreateLoadBalancerHTTPSListenerBuilder healthyThreshold(final int healthyThreshold);
+
+        /**
+        *
+        健康检查连续失败多少次后，将后端服务器的健康检查状态由success判定为fail。
+        取值：2-10。
+        说明 在HealthCheck值为on时才会有效。
+        */
+        @QueryParam("UnhealthyThreshold")
+        CreateLoadBalancerHTTPSListenerBuilder unhealthyThreshold(final int unhealthyThreshold);
+
+        /**
+        *
+        接收来自运行状况检查的响应需要等待的时间。如果后端ECS在指定的时间内没有正确响应，则判定为健康检查失败。在HealthCheck值为on时才会有效。
+        取值：1-300（秒）。
+        说明
+        如果HealthCHeckTimeout的值小于HealthCheckInterval的值，则HealthCHeckTimeout无效，超时时间为HealthCheckInterval的值。
+        在HealthCheck值为on时才会有效。
+        */
+        @QueryParam("HealthCheckTimeout")
+        CreateLoadBalancerHTTPSListenerBuilder healthCheckTimeout(final int healthCheckTimeout);
+
+        /**
+        *
+        健康检查使用的端口。
+        取值：1-65535。
+        说明 当HealthCheck值为on时才会有效。
+        */
+        @QueryParam("HealthCheckConnectPort")
+        CreateLoadBalancerHTTPSListenerBuilder healthCheckConnectPort(final int healthCheckConnectPort);
+
+        /**
+        *
+        健康检查的时间间隔。
+        取值：1-50（秒）。
+        说明 在HealthCheck值为on时才会有效。
+        */
+        @QueryParam("HealthCheckInterval")
+        CreateLoadBalancerHTTPSListenerBuilder healthCheckInterval(final int healthCheckInterval);
+
+        /**
+        *
+        健康检查正常的HTTP状态码，多个状态码用逗号（,）分割。默认值为http_2xx。
+        取值：http_2xx | http_3xx | http_4xx | http_5xx。
+        说明 在HealthCheck值为on时才会有效。
+        */
+        @QueryParam("HealthCheckHttpCode")
+        CreateLoadBalancerHTTPSListenerBuilder healthCheckHttpCode(final String healthCheckHttpCode);
+
+        /**
+        *
+        服务器组ID。
+        */
+        @QueryParam("VServerGroupId")
+        CreateLoadBalancerHTTPSListenerBuilder vServerGroupId(final String vServerGroupId);
+
+        /**
+        *
+        CA证书ID。
+        若既上传CA证书又上传服务器证书，即采用双向认证。
+        若用户只上传服务器证书，即为单向认证。
+        */
+        @QueryParam("CACertificateId")
+        CreateLoadBalancerHTTPSListenerBuilder cACertificateId(final String cACertificateId);
+
+        /**
+        *
+        是否通过SLB-IP头字段获取来访者真实IP。
+        取值：on |off （默认值）。
+        */
+        @QueryParam("XForwardedFor_SLBIP")
+        CreateLoadBalancerHTTPSListenerBuilder xForwardedFor_SLBIP(final String xForwardedFor_SLBIP);
+
+        /**
+        *
+        是否通过SLB-ID头字段获取SLB实例ID。
+        取值：on | off（默认值）。
+        */
+        @QueryParam("XForwardedFor_SLBID")
+        CreateLoadBalancerHTTPSListenerBuilder xForwardedFor_SLBID(final String xForwardedFor_SLBID);
+
+        /**
+        *
+        是否通过X-Forwarded-Proto头字段获取SLB的监听协议。
+        取值：on | off（默认值）。
+        */
+        @QueryParam("XForwardedFor_proto")
+        CreateLoadBalancerHTTPSListenerBuilder xForwardedFor_proto(final String xForwardedFor_proto);
+
+        /**
+        *
+        是否开启Gzip压缩，对特定文件类型进行压缩。
+        取值：on（默认值）| off。
+        */
+        @QueryParam("Gzip")
+        CreateLoadBalancerHTTPSListenerBuilder gzip(final String gzip);
+
+        /**
+        *
+        监听绑定的访问策略组ID。
+        说明 当AclStatus参数的值为on时，该参数必选。
+        */
+        @QueryParam("AclId")
+        CreateLoadBalancerHTTPSListenerBuilder aclId(final String aclId);
+
+        /**
+        *
+        访问控制类型：
+        white： 仅转发来自所选访问控制策略组中设置的IP地址或地址段的请求，白名单适用于应用只允许特定IP访问的场景。
+        设置白名单存在一定业务风险。
+        一旦设置白名单，就只有白名单中的IP可以访问负载均衡监听。
+        如果开启了白名单访问，但访问策略组中没有添加任何IP，则负载均衡监听不会转发请求。
+        black： 来自所选访问控制策略组中设置的IP地址或地址段的所有请求都不会转发，黑名单适用于应用只限制某些特定IP访问的场景。
+        如果开启了黑名单访问，但访问策略组中没有添加任何IP，则负载均衡监听会转发全部请求。
+        说明 当AclStatus参数的值为on时，该参数有效。
+        */
+        @QueryParam("AclType")
+        CreateLoadBalancerHTTPSListenerBuilder aclType(final String aclType);
+
+        /**
+        *
+        是否开启访问控制功能。
+        取值：on | off（默认值）。
+        */
+        @QueryParam("AclStatus")
+        CreateLoadBalancerHTTPSListenerBuilder aclStatus(final String aclType);
+
+        /**
+        *
+        设置监听的描述信息。
+        长度限制为1-80个字符，允许包含字母、数字、“-”、“/”、“.”和“_”等字符。支持中文描述。
+        */
+        @QueryParam("Description")
+        CreateLoadBalancerHTTPSListenerBuilder description(final String description);
+
+        /**
+        *
+        指定连接空闲超时时间，取值范围为1-60秒，默认值为15秒。
+        在超时时间内一直没有访问请求，负载均衡会暂时中断当前连接，直到一下次请求来临时重新建立新的连接。
+        */
+        @QueryParam("IdleTimeout")
+        CreateLoadBalancerHTTPSListenerBuilder idleTimeout(final int idleTimeout);
+
+        /**
+        *
+        指定请求超时时间，取值范围为1-180秒，默认值为60秒。
+        在超时时间内后端服务器一直没有响应，负载均衡将放弃等待，给客户端返回HTTP 504错误码。
+        */
+        @QueryParam("RequestTimeout")
+        CreateLoadBalancerHTTPSListenerBuilder requestTimeout(final int requestTimeout);
+
+        /**
+        *
+        是否开启HTTP/2特性。
+        取值：on（默认值）|off。
+        */
+        @QueryParam("EnableHttp2")
+        CreateLoadBalancerHTTPSListenerBuilder enableHttp2(final String enableHttp2);
+
+        /**
+        *
+        只有性能保障型实例才可以指定TLSCipherPolicy参数，每种policy定义了一种安全策略，安全策略包含HTTPS可选的TLS协议版本和配套的加密算法套件。
+        目前支持以下四种安全策略，详细区别请参见TLS安全策略差异说明，请根据实际情况选择对应的policy。
+        tls_cipher_policy_1_0：
+        支持TLS版本： TLSv1.0、TLSv1.1和TLSv1.2。
+        支持加密算法套件：ECDHE-RSA-AES128-GCM-SHA256、ECDHE-RSA-AES256-GCM-SHA384、ECDHE-RSA-AES128-SHA256、ECDHE-RSA-AES256-SHA384、AES128-GCM-SHA256、AES256-GCM-SHA384、AES128-SHA256、AES256-SHA256、ECDHE-RSA-AES128-SHA、ECDHE-RSA-AES256-SHA、AES128-SHA、AES256-SHA和DES-CBC3-SHA。
+        tls_cipher_policy_1_1：
+        支持TLS版本： TLSv1.1和TLSv1.2。
+        支持加密算法套件：ECDHE-RSA-AES128-GCM-SHA256、ECDHE-RSA-AES256-GCM-SHA384、ECDHE-RSA-AES128-SHA256、ECDHE-RSA-AES256-SHA384、AES128-GCM-SHA256、AES256-GCM-SHA384、AES128-SHA256、AES256-SHA256、ECDHE-RSA-AES128-SHA、ECDHE-RSA-AES256-SHA、AES128-SHA、AES256-SHA和DES-CBC3-SHA。
+        tls_cipher_policy_1_2
+        支持TLS版本：TLSv1.2。
+        支持加密算法套件：ECDHE-RSA-AES128-GCM-SHA256、ECDHE-RSA-AES256-GCM-SHA384、ECDHE-RSA-AES128-SHA256、ECDHE-RSA-AES256-SHA384、AES128-GCM-SHA256、AES256-GCM-SHA384、AES128-SHA256、AES256-SHA256、ECDHE-RSA-AES128-SHA、ECDHE-RSA-AES256-SHA、AES128-SHA、AES256-SHA和DES-CBC3-SHA。
+        tls_cipher_policy_1_2_strict
+        支持TLS版本：TLSv1.2。
+        支持加密算法套件：ECDHE-RSA-AES128-GCM-SHA256、ECDHE-RSA-AES256-GCM-SHA384、ECDHE-RSA-AES128-SHA256、ECDHE-RSA-AES256-SHA384、ECDHE-RSA-AES128-SHA和ECDHE-RSA-AES256-SHA。
+        */
+        @QueryParam("TLSCipherPolicy")
+        CreateLoadBalancerHTTPSListenerBuilder tLSCipherPolicy(final String tLSCipherPolicy);
 
         @GET
         @ConstParams({"Action", "CreateLoadBalancerHTTPSListener"})

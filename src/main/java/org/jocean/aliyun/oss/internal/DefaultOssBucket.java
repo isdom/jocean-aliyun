416d92db7bb07d401d81552f4096d5e84e53005f
ming.ma <isdom@qq.com>
2020-12-16 10:09:07 +0800

    add more log for DefaultOssBucket

--- src/main/java/org/jocean/aliyun/oss/internal/DefaultOssBucket.java
+++ src/main/java/org/jocean/aliyun/oss/internal/DefaultOssBucket.java
@@ -3,18 +3,32 @@ package org.jocean.aliyun.oss.internal;
 import org.jocean.aliyun.oss.OssAPI.Bucketable;
 import org.jocean.aliyun.oss.OssAPI.Endpointable;
 import org.jocean.aliyun.oss.OssBucket;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 
 class DefaultOssBucket implements OssBucket {
+    private static final Logger LOG = LoggerFactory.getLogger(DefaultOssBucket.class);
+
     @Override
     public <BUILDER> BUILDER apply(final Endpointable<BUILDER> endpointable) {
+        LOG.debug("apply endpoint {} to {}", _ossEndpoint, endpointable);
         return endpointable.endpoint(_ossEndpoint);
     }
 
     @Override
     public <BUILDER> BUILDER apply(final Bucketable<BUILDER> bucketable) {
+        LOG.debug("apply endpoint {} & bucket {} to {}", _ossEndpoint, _ossBucket, bucketable);
         bucketable.endpoint(_ossEndpoint);
         return bucketable.bucket(_ossBucket);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append("DefaultOssBucket [ossEndpoint=").append(_ossEndpoint).append(", ossBucket=")
+                .append(_ossBucket).append("]");
+        return builder.toString();
     }
 
     @Value("${oss.endpoint}")
